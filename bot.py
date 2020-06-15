f2e2b91ce363696a9a8122d40ae3287031c99702
jangui <jaimitino@gmail.com>
2020-06-15 00:49:41 -0400

    found a nice scraper

--- bot.py
+++ bot.py
@@ -1,6 +1,30 @@
 from instapy import InstaPy
 from time import sleep
 
+def follow(username):
+    try:
+        session.browser.get('https://www.instagram.com/' + username + '/')
+        sleep(2)
+        buttons = session.browser.find_elements_by_css_selector('button')
+        follow_button = buttons[1]
+        follow_button.click()
+    except:
+        print(f"failed to follow {username}")
+
+def unfollow(username):
+    try:
+        session.browser.get('https://www.instagram.com/' + username + '/')
+        sleep(2)
+        buttons = session.browser.find_elements_by_css_selector('button')
+        unfollow_button = buttons[2]
+        unfollow_button.click()
+        sleep(1)
+        confirm_button = session.browser.find_element_by_xpath('//button[text() = "Unfollow"]')
+        confirm_button.click()
+    except:
+        print(f"failed to unfollow {username}")
+
+
 username = "espanollie"
 password = "6vzvmaHVQ9UWkV5ptE72N7LUpb7g5fMT"
 
@@ -8,30 +32,22 @@ session = InstaPy(username=username, password=password)
 #session = InstaPy(username=username, password=password, headless_browser=True)
 session.login()
 
-def follow_user(username):
-        session.browser.get('https://www.instagram.com/' + username + '/')
-        sleep(2)
-        buttons = session.browser.find_elements_by_css_selector('button')
-        follow_button = buttons[1]
-        follow_button.click()
-        """
-        if (follow_button.text != 'Following'):
-            follow_button.click()
-            sleep(5)
-        else:
-            print("You are already following this user")
-        """
-def unfollow_user(username):
-        session.browser.get('https://www.instagram.com/' + username + '/')
-        sleep(2)
-        buttons = session.browser.find_elements_by_css_selector('button')
-        unfollow_button = buttons[2]
-        unfollow_button.click()
-        confirm_button = session.browser.find_element_by_xpath('//button[text() = "Unfollow"]')
-        confirm_button.click()
+users = ["el.jorgeto.por.ejemplo", "drake", "jaim__d", "djkhaled"]
 
-#unfollow_user("newyorknico")
-#follow_user("newyorknico")
-sleep(5)
+for user in users:
+    follow(user)
+    sleep(2)
+
+for user in users:
+    unfollow(user)
+    sleep(2)
+
+for user in users:
+    follow(user)
+    sleep(2)
+
+for user in users:
+    unfollow(user)
+    sleep(2)
 
 session.end()
